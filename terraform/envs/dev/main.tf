1196cf6656a28fb91930b545adfb7b8f937020c0
Raghukongala <kongalaraghu406@gmail.com>
2026-04-09 23:59:39 +0530

    Update main.tf

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -1,6 +1,19 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
+
+# ---------------- VPC ----------------
 
 module "vpc" {
   source = "../../modules/vpc"
@@ -13,6 +26,8 @@ module "vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
+# ---------------- EKS ----------------
+
 module "eks" {
   source = "../../modules/eks"
 
@@ -20,6 +35,8 @@ module "eks" {
   vpc_id          = module.vpc.vpc_id
   private_subnets = module.vpc.private_subnets
 }
+
+# ---------------- ECR ----------------
 
 module "ecr" {
   source = "../../modules/ecr"
