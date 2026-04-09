0b4f6c658a1d62f6d806b5f7d33241e27813eb17
Raghukongala <kongalaraghu406@gmail.com>
2026-04-10 00:15:07 +0530

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -2,8 +2,8 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+  name               = var.cluster_name
+  kubernetes_version = "1.29"
 
   vpc_id     = var.vpc_id
   subnet_ids = var.private_subnets
