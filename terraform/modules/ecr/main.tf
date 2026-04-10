7b1d4edad61cac1644750ec437276b698363f223
Raghukongala <kongalaraghu406@gmail.com>
2026-04-10 09:59:16 +0530

    Update main.tf

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -4,15 +4,14 @@ resource "aws_ecr_repository" "repos" {
   name                 = each.value
   image_tag_mutability = "MUTABLE"
 
-  # ✅ DEV SAFE: allows destroy even if images exist
   force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
   }
 
-  # ✅ DEV ONLY: prevents accidental issues during iterations
   lifecycle {
     prevent_destroy = false
+    ignore_changes  = all
   }
 }
