415fa482b1cca98067247f58f54b63199587c368
Raghukongala <kongalaraghu406@gmail.com>
2026-04-10 10:30:59 +0530

    Update main.tf

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -27,7 +27,7 @@ module "vpc" {
 module "eks" {
   source = "../../modules/eks"
 
-  cluster_name    = "dev-eks-cluster"
+  cluster_name    = "dev-eks-clusterv2"
   vpc_id          = module.vpc.vpc_id
   private_subnets = module.vpc.private_subnets
 }
